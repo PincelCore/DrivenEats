7cdd5aea6c0ba5fd082fa8d5523fb2517b80f3dc
PincelCore <alves.davi63@gmail.com>
2023-03-31 14:43:28 -0300

    Finishing touches

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-let meal;
-let mealprice;
+let mealName;
+let mealPrice;
 let mealSelected;
 
-let drink;
-let drinkprice;
+let drinkName;
+let drinkPrice;
 let drinkSelected;
 
-let dessert;
-let dessertprice;
+let dessertName;
+let dessertPrice;
 let dessertSelected;
+
+let total_order;
 
 function enable_button() {
   var button = document.querySelector(".button");
@@ -27,201 +29,125 @@ function check_selection(mealSelected, drinkSelected, dessertSelected) {
     }
   }
 
-function choosemeal1() {
-    document.getElementById("meal1").style.borderColor = "green";
-    document.getElementById("meal1").querySelector(".checkmark").style.display = "block";
-
-    document.getElementById("meal2").style.borderColor = "transparent";
-    document.getElementById("meal2").querySelector(".checkmark").style.display = "none";
-
-    document.getElementById("meal3").style.borderColor = "transparent";
-    document.getElementById("meal3").querySelector(".checkmark").style.display = "none";
-
-    meal = "Frango Yin Yang";
-    mealprice = 14.90;
-    mealSelected = true;
-
-    check_selection(mealSelected, drinkSelected, dessertSelected);
+  let selectedMeal = null;
+  const meals = document.querySelectorAll(".meal");
+  
+  function chooseMeal(mealId) {
+    if (selectedMeal !== null) {
+      selectedMeal.style.borderColor = "transparent";
+      selectedMeal.querySelector(".checkmark").style.display = "none";
     }
-function choosemeal2() {
-    document.getElementById("meal1").style.borderColor = "transparent";
-    document.getElementById("meal1").querySelector(".checkmark").style.display = "none";
+    const meal = document.getElementById(mealId);
+    meal.style.borderColor = "green";
+    meal.querySelector(".checkmark").style.display = "block";
+    selectedMeal = meal;
+  
+    mealName = meal.querySelector("h4").innerText;
+  
+    mealPrice = Number.parseFloat(meal.querySelector(".price").innerText.replace("R$ ", "").replace(",", "."));
 
-    document.getElementById("meal2").style.borderColor = "green";
-    document.getElementById("meal2").querySelector(".checkmark").style.display = "block";
-
-    document.getElementById("meal3").querySelector(".checkmark").style.display = "none";
-    document.getElementById("meal3").style.borderColor = "transparent";
-
-    meal = "Frango Yin Yang";
-    mealprice = 14.90;
     mealSelected = true;
 
     check_selection(mealSelected, drinkSelected, dessertSelected);
+  }
+  
+
+  let selectedDrink = null;
+  const drinks = document.querySelectorAll(".drink");
+  
+  function chooseDrink(drinkId) {
+    if (selectedDrink !== null) {
+      selectedDrink.style.borderColor = "transparent";
+      selectedDrink.querySelector(".checkmark").style.display = "none";
     }
-function choosemeal3() {
-    document.getElementById("meal1").style.borderColor = "transparent";
-    document.getElementById("meal1").querySelector(".checkmark").style.display = "none";
+    const drink = document.getElementById(drinkId);
+    drink.style.borderColor = "green";
+    drink.querySelector(".checkmark").style.display = "block";
+    selectedDrink = drink;
+  
+    drinkName = drink.querySelector("h4").innerText;
+  
+    drinkPrice = Number.parseFloat(drink.querySelector(".price").innerText.replace("R$ ", "").replace(",", "."));
 
-    document.getElementById("meal2").querySelector(".checkmark").style.display = "none";
-    document.getElementById("meal2").style.borderColor = "transparent";
-
-    document.getElementById("meal3").querySelector(".checkmark").style.display = "block";
-    document.getElementById("meal3").style.borderColor = "green";
-
-    meal = "Frango Yin Yang";
-    mealprice = 14.90;
-    mealSelected = true;
-
-    check_selection(mealSelected, drinkSelected, dessertSelected);
-    } 
-
-function choosedrink1(){
-    document.getElementById("drink1").style.borderColor = "green";
-    document.getElementById("drink1").querySelector(".checkmark").style.display = "block";
-
-    document.getElementById("drink2").style.borderColor = "transparent";
-    document.getElementById("drink2").querySelector(".checkmark").style.display = "none";
-
-    document.getElementById("drink3").style.borderColor = "transparent";
-    document.getElementById("drink3").querySelector(".checkmark").style.display = "none";
-
-    drink = "Coquinha gelada";
-    drinkprice = 4.90;
     drinkSelected = true;
 
     check_selection(mealSelected, drinkSelected, dessertSelected);
-}
-function choosedrink2(){
-    document.getElementById("drink1").style.borderColor = "transparent";
-    document.getElementById("drink1").querySelector(".checkmark").style.display = "none";
-
-    document.getElementById("drink2").style.borderColor = "green";
-    document.getElementById("drink2").querySelector(".checkmark").style.display = "block";
-
-    document.getElementById("drink3").style.borderColor = "transparent";
-    document.getElementById("drink3").querySelector(".checkmark").style.display = "none";
-
-    drink = "Coquinha gelada";
-    drinkprice = 4.90;
-    drinkSelected = true;
-
-    check_selection(mealSelected, drinkSelected, dessertSelected);
-}
-function choosedrink3(){
-    document.getElementById("drink1").style.borderColor = "transparent";
-    document.getElementById("drink1").querySelector(".checkmark").style.display = "none";
-
-    document.getElementById("drink2").style.borderColor = "transparent";
-    document.getElementById("drink2").querySelector(".checkmark").style.display = "none";
-
-    document.getElementById("drink3").style.borderColor = "green";
-    document.getElementById("drink3").querySelector(".checkmark").style.display = "block";
-
-    drink = "Coquinha gelada";
-    drinkprice = 4.90;
-    drinkSelected = true;
-
-    check_selection(mealSelected, drinkSelected, dessertSelected);
-}
-function choosedessert1(){
-    document.getElementById("dessert1").style.borderColor = "green";
-    document.getElementById("dessert1").querySelector(".checkmark").style.display = "block";
-
-    document.getElementById("dessert2").style.borderColor = "transparent";
-    document.getElementById("dessert2").querySelector(".checkmark").style.display = "none";
-
-    document.getElementById("dessert3").style.borderColor = "transparent";
-    document.getElementById("dessert3").querySelector(".checkmark").style.display = "none";
-
-    dessert = "Pudim";
-    dessertprice = 7.90;
-    dessertSelected = true;
-
-    check_selection(mealSelected, drinkSelected, dessertSelected);
-
-}
-function choosedessert2(){
-    document.getElementById("dessert1").style.borderColor = "transparent";
-    document.getElementById("dessert1").querySelector(".checkmark").style.display = "none";
-
-    document.getElementById("dessert2").style.borderColor = "green";
-    document.getElementById("dessert2").querySelector(".checkmark").style.display = "block";
-
-    document.getElementById("dessert3").style.borderColor = "transparent";
-    document.getElementById("dessert3").querySelector(".checkmark").style.display = "none";
-
-    dessert = "Pudim";
-    dessertprice = 7.90;
-    dessertSelected = true;
-
-    check_selection(mealSelected, drinkSelected, dessertSelected);
-}
-function choosedessert3(){
-    document.getElementById("dessert1").style.borderColor = "transparent";
-    document.getElementById("dessert1").querySelector(".checkmark").style.display = "none";
-
-    document.getElementById("dessert2").style.borderColor = "transparent";
-    document.getElementById("dessert2").querySelector(".checkmark").style.display = "none";
-
-    document.getElementById("dessert3").style.borderColor = "green";
-    document.getElementById("dessert3").querySelector(".checkmark").style.display = "block";
-
-    dessert = "Pudim";
-    dessertprice = 7.90;
-    dessertSelected = true;
-
-    check_selection(mealSelected, drinkSelected, dessertSelected);
-}
-function open_whatsapp() {
-  let total_order = mealprice + drinkprice + dessertprice;
-  total_order = total_order.toFixed(2).replace(".", ",");
-
-  const name = prompt('Informe seu nome: ');
-  const adress = prompt('Informe seu endereço: ');
-
-  let mensagem =
-    "Olá, gostaria de fazer o pedido:\r\n" +
-    "- Prato: " + meal + "\r\n" +
-    "- Bebida: " + drink + "\r\n" +
-    "- Sobremesa: " + dessert + "\r\n" +
-    "Total: R$ " + total_order + "\r\n" +
-    "\r\n" +
-    "Nome: " + name + "\r\n" +
-    "Endereço: " + adress
-
-  window.open("https://wa.me/+5521999999999?text=" + encodeURIComponent(mensagem));
-}
-
-function place_order() {
-  if (!document.getElementById('order-button').classList.contains('enabled')) {
-    return;
   }
 
-  const confirmationMenu = document.querySelector('.confirmation-menu');
-  const pageCover = document.querySelector('.page-cover');
-  const confirmButton = document.querySelector('#confirm-button');
-  const cancelButton = document.querySelector('#cancel-button');
 
-  pageCover.style.display = 'block';
-
-  let total_order = mealprice + drinkprice + dessertprice;
-  total_order = total_order.toFixed(2).replace(".", ",");
-
-  document.getElementById("meal").innerHTML = meal;
-  document.getElementById("drink").innerHTML = drink;
-  document.getElementById("dessert").innerHTML = dessert;
-  document.getElementById("meal-price").innerHTML = mealprice.toFixed(2).replace(".", ",");;
-  document.getElementById("drink-price").innerHTML = drinkprice.toFixed(2).replace(".", ",");
-  document.getElementById("dessert-price").innerHTML = dessertprice.toFixed(2).replace(".", ",");
-  document.getElementById("total-price").innerHTML = total_order;
-
-  confirmButton.addEventListener('click', open_whatsapp);
-
-  cancelButton.addEventListener('click', function() {
-    pageCover.style.display = 'none';
-    confirmationMenu.style.display = 'none';
-  });
+  let selectedDessert = null;
+  const desserts = document.querySelectorAll(".dessert");
   
-  confirmationMenu.style.display = 'block';
+  function chooseDessert(dessertId) {
+    if (selectedDessert !== null) {
+      selectedDessert.style.borderColor = "transparent";
+      selectedDessert.querySelector(".checkmark").style.display = "none";
+    }
+    const dessert = document.getElementById(dessertId);
+    dessert.style.borderColor = "green";
+    dessert.querySelector(".checkmark").style.display = "block";
+    selectedDessert = dessert;
+  
+    dessertName = dessert.querySelector("h4").innerText;
+  
+    dessertPrice = Number.parseFloat(dessert.querySelector(".price").innerText.replace("R$ ", "").replace(",", "."));
+
+    dessertSelected = true;
+
+    check_selection(mealSelected, drinkSelected, dessertSelected);
+  }
+
+  function open_whatsapp() {
+
+    const name = prompt('Informe seu nome: ');
+    const adress = prompt('Informe seu endereço: ');
+
+    if (name.trim() === '' || adress.trim() === ''){
+      alert("Insira um nome ou endereço correto");
+      return;
+    }
+
+    let message =
+      "Olá, gostaria de fazer o pedido:\r\n" +
+      "- Prato: " + mealName + "\r\n" +
+      "- Bebida: " + drinkName + "\r\n" +
+      "- Sobremesa: " + dessertName + "\r\n" +
+      "Total: R$ " + total_order + "\r\n" +
+      "\r\n" +
+      "Nome: " + name + "\r\n" +
+      "Endereço: " + adress
+
+    window.open("https://wa.me/+5521999999999?text=" + encodeURIComponent(message));
+}
+
+  function place_order() {
+    if (!document.getElementById('order-button').classList.contains('enabled')) {
+      return;
+    }
+
+    const confirmationMenu = document.querySelector('.confirmation-menu');
+    const pageCover = document.querySelector('.page-cover');
+    const confirmButton = document.querySelector('#confirm-button');
+    const cancelButton = document.querySelector('#cancel-button');
+
+    pageCover.style.display = 'block';
+
+    total_order = (mealPrice + drinkPrice + dessertPrice).toFixed(2).replace(".", ",");
+
+    document.getElementById("meal").innerHTML = mealName;
+    document.getElementById("drink").innerHTML = drinkName;
+    document.getElementById("dessert").innerHTML = dessertName;
+    document.getElementById("meal-price").innerHTML = mealPrice.toFixed(2).replace(".", ",");;
+    document.getElementById("drink-price").innerHTML = drinkPrice.toFixed(2).replace(".", ",");
+    document.getElementById("dessert-price").innerHTML = dessertPrice.toFixed(2).replace(".", ",");
+    document.getElementById("total-price").innerHTML = total_order;
+
+    confirmButton.addEventListener('click', open_whatsapp);
+
+    cancelButton.addEventListener('click', function() {
+      pageCover.style.display = 'none';
+      confirmationMenu.style.display = 'none';
+    });
+  
+    confirmationMenu.style.display = 'block';
 }
